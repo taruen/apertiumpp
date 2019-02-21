9d1e0d0d3e5205746af967140d205da9302a082c
Ilnar Salimzianov <ilnar@selimcan.org>
2019-02-21 18:34:04 +0300

    declare 'rash' package a depenendancy

--- apertiumpp/info.rkt
+++ apertiumpp/info.rkt
@@ -1,8 +1,8 @@
 #lang info
 (define collection "apertiumpp")
-(define deps '("base"))
+(define deps '("base" "rash"))
 (define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
 (define scribblings '(("scribblings/apertiumpp.scrbl" ())))
 (define pkg-desc "Description Here")
 (define version "0.0")
-(define pkg-authors '(selimcan))
+(define pkg-authors '("Ilnar Salimzianov"))
