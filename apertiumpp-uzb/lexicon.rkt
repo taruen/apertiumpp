294f7328fd9ab3715ff6a6c6db40df43eb6ef862
Ilnar Salimzianov <ilnar@selimcan.org>
2019-07-03 13:19:02 +0300

    backup

---
+++ apertiumpp-uzb/lexicon.rkt
@@ -0,0 +1,601 @@
+a:a CC-PROTIV ; ! 
+a:a IJ ; ! 
+%-a:%-a QST ; !
+%-ya:%-ya QST ; !
+abad:abad ADV ; ! 
+abad:abad N1 ; ! 
+abadiy:abadiy A1 ; ! 
+abadiylash:abadiylash N1-VN ; ! 
+abadiylashtir:abadiylashtir V-TV-CAUS ; ! 
+abadiylik:abadiyli%{K%} N1 ;  ! 
+abadiya:abadiya A2 ; !
+abadiyan:abadiyan ADV ; ! 
+abadiyat:abadiyat N1 ; ! 
+abajur:abajur N1 ; ! 
+abas:abas A1 ; ! 
+abgor:abgor A1 ; ! 
+abgorlik:abgorli%{K%} N1 ;  ! 
+abdol:abdol A2 ; ! 
+aberratsion:aberratsion A2 ; ! 
+aberratsiya:aberratsiya N1 ; ! 
+abjad:abjad N1 ; ! 
+abjaq:abjaq N1 ; ! 
+abjir:abjir A2 ; ! 
+abjirlik:abjirli%{K%} N1 ;  ! 
+abzal:abzal N1 ; ! 
+abzalla:abzalla V-TV ; ! 
+abzats:abzats N1 ; !
+abira:abira N1 ; ! 
+ablaq:ablaq A2 ; ! 
+ablah:ablah N1 ; ! 
+ablahlarcha:ablahlarcha ADV ; ! 
+ablahlik:ablahli%{K%} N1 ;  ! 
+ablahona:ablahona A1 ; ! 
+abonement:abonement N1 ; !
+abonent:abonent N1 ; ! 
+abort:abort N1 ; !
+abortiv:abortiv A2 ; ! 
+abort% qil:abort% qil V-IV ; ! 
+abr:abr N1 ; ! 
+abraziv:abraziv N1 ; ! 
+abrishim:abrishim N1 ; ! 
+abroʻ:abroʻ N1 ; !
+abroʻkamon:abroʻkamon A2 ; ! 
+abroʻkamon:abroʻkamon N1 ; ! 
+absolut:absolut A1 ; ! 
+absolut:absolut N1 ; ! 
+absolyutizm:absolyutizm N1 ; ! 
+abstrakt:abstrakt A2 ; ! 
+abstraktla:abstraktla V-TV ; ! 
+abstraktlashtir:abstraktlashtir V-TV-CAUS ; ! 
+abstraktlik:abstraktli%{K%} N1 ;  ! 
+abstraksiya:abstraksiya N1 ; ! 
+abssess:abssess N1 ; ! 
+abssissa:abssissa N1 ; ! 
+abxaz:abxaz N1-NAT ; ! 
+av:av MIST ; !
+avayla:avayla V-TV ; ! 
+avaylab:avaylab ADV ; ! 
+avangard:avangard N1 ; ! 
+avangardlik:avangardli%{K%} N1 ;  ! 
+avans:avans N1 ; ! 
+avantyura:avantyura N1 ; ! 
+avantyurachi:avantyurachi N1 ; ! 
+avantyurizm:avantyurizm N1 ; ! 
+avantyurist:avantyurist N1 ; ! 
+avantyuristik:avantyuristik A2 ; ! 
+avantyuristlik:avantyuristli%{K%} N1 ;  ! 
+avar:avar N1-NAT ; ! 
+avariya:avariya N1 ; ! 
+avaxta:avaxta N1 ; ! 
+avagʻa:avagʻa N1 ; ! 
+avbosh:avbosh N1 ; ! 
+avboshlik:avboshli%{K%} N1 ;  ! 
+avval:avval A1 ; ! 
+avval:avval ADV-ATTR ; ! 
+avval:avval N1 ; !
+avval:avval POST-ABL ; ! 
+avvalan:avvalan ADV ; ! 
+avvalgidek:avvalgidek ADV ; ! 
+avvalgicha:avvalgicha ADV ; ! 
+avvallari:avvallari ADV ; ! 
+avvalo:avvalo ADV ; ! 
+avg%.:avg%. ABBR ;
+avg:avg ABBR ; ! "august"
+avgust:avgust N1 ; ! "August"
+avj:avj N1 ; ! 
+avjlantir:avjlantir V-TV-CAUS ; ! 
+avjli:avjli A1 ; ! 
+avjsiz:avjsiz A1 ; ! 
+avzo:avzo N1 ; ! 
+avzoi:avzoi N1 ; ! 
+aviabaza:aviabaza N1 ; ! 
+aviabomba:aviabomba N1 ; ! 
+aviadesant:aviadesant N1 ; ! 
+aviadesantchi:aviadesantchi N1 ; ! 
+aviazavod:aviazavod N1 ; ! 
+aviakonstruktor:aviakonstruktor N1 ; ! 
+avialiniya:avialiniya N1 ; ! 
+aviamodel:aviamodel N1 ; ! 
+aviamodelchi:aviamodelchi N1 ; ! 
+aviamodelchilik:aviamodelchili%{K%} N1 ;  ! 
+aviamotor:aviamotor N1 ; !
+aviamotorsozlik:aviamotorsozlik N1 ; ! 
+avianos:avianos N1 ; ! 
+avianosets:avianosets N1 ; ! 
+aviapochta:aviapochta N1 ; ! 
+aviatsion:aviatsion A2 ; ! 
+aviatsiya:aviatsiya N1 ; ! 
+avliyo:avliyo N1 ; ! 
+avliyonamo:avliyonamo A2 ; ! 
+avlo:avlo A1 ; ! 
+avlod:avlod N1 ; ! "child/son/daughter" 
+avlod-ajdod:avlod-ajdod N1 ; ! 
+avom:avom N1 ; ! 
+avra:avra N1 ; ! 
+avra%-astar:avra%-astar N1 ; ! 
+avral:avral N1 ; ! 
+avra:avra V-TV ; ! 
+avrat:avrat N1 ; ! 
+avratpoʻsh:avratpoʻsh N1 ; ! 
+avrash:avrash N1-VN ; ! 
+avrash%-aldash:avrash%-aldash N1-VN ; !
+avrogʻich:avrogʻich N1 ; ! 
+avroqchi:avroqchi N1 ; ! 
+avsat:avsat A1 ; ! 
+avtobaza:avtobaza N1 ; ! 
+avtobiografik:avtobiografik A2 ; ! 
+avtobiografiya:avtobiografiya N1 ; ! 
+avtoblokirovka:avtoblokirovka N1 ; ! 
+avtobus:avtobus N1 ; ! "bus"
+avtogen:avtogen N1 ; ! 
+avtograf:avtograf N1 ; ! 
+avtografik:avtografik A2 ; ! 
+avtozavod:avtozavod N1 ; ! 
+avtokamera:avtokamera N1 ; ! 
+avtokolonna:avtokolonna N1 ; ! 
+avtol:avtol N1 ; ! 
+avtomat:avtomat N1 ; ! 
+avtomatik:avtomatik A2 ; ! 
+avtomatlash:avtomatlash V-IV ; ! 
+avtomatlashtir:avtomatlashtir V-TV-CAUS ; ! 
+avtomatlashtirish:avtomatlashtirish N1-VN ; ! 
+avtomatchi:avtomatchi N1 ; ! 
+avtomashina:avtomashina N1 ; ! 
+avtomobil:avtomobil N1 ; ! "car"
+avtomobilsozlik:avtomobilsozli%{K%} N1 ;  ! 
+avtonom:avtonom A2 ; ! "autonomous" 
+avtonomist:avtonomist N1 ; ! 
+avtonomiya:avtonomiya N1 ; ! 
+avtonomiyali:avtonomiyali A1 ; ! 
+avtonomiyachi:avtonomiyachi N1 ; ! 
+avtonomlik:avtonomli%{K%} N1 ;  ! 
+avtopark:avtopark N1 ; ! 
+avtopoyga:avtopoyga N1 ; ! 
+avtor:avtor N1 ; ! 
+avtoremont:avtoremont N1 ; ! 
+avtoreferat:avtoreferat N1 ; ! 
+avtorlik:avtorli%{K%} N1 ;  ! 
+avtoruchka:avtoruchka N1 ; ! 
+avtosugʻorgʻich:avtosugʻorgʻich N1 ; ! 
+avtotransport:avtotransport N1 ; ! 
+avtoxoʻjalik:avtoxoʻjali%{K%} N1 ;  ! 
+avtochana:avtochana N1 ; ! 
+agar:agar CS ; ! "if"
+agarda:agarda CS ; ! "if"
+agar% magar:agar% magar CS ; ! 
+agglyutinativ:agglyutinativ A2 ; ! 
+agglyutinatsiya:agglyutinatsiya N1 ; ! 
+agent:agent N1 ; ! 
+agentlik:agentli%{K%} N1 ;  ! 
+agentura:agentura N1 ; ! 
+agitator:agitator N1 ; ! 
+agitatorlik:agitatorli%{K%} N1 ;  ! 
+agitatsion:agitatsion A2 ; ! 
+agitatsiya:agitatsiya N1 ; ! 
+agitpunkt:agitpunkt N1 ; ! 
+agnostik:agnostik A2 ; ! 
+agnostik:agnostik N1 ; ! 
+agnostitsizm:agnostitsizm N1 ; ! 
+agrar:agrar A2 ; ! "agrarian" 
+agrarchi:agrarchi N1 ; ! 
+agregat:agregat N1 ; ! "agregate"
+agressiv:agressiv A2 ; ! 
+agressivlik:agressivli%{K%} N1 ;  ! 
+agressiya:agressiya N1 ; ! 
+agressiyachi:agressiyachi N1 ; ! 
+agressiyachilik:agressiyachili%{K%} N1 ;  ! 
+agressor:agressor N1 ; ! 
+agressorlik:agressorli%{K%} N1 ;  ! 
+agrikultura:agrikultura N1 ; ! 
+agrobiologik:agrobiologik A2 ; ! 
+agrobiologiya:agrobiologiya N1 ; ! 
+agrokultura:agrokultura N1 ; ! 
+agromeliorativ:agromeliorativ A2 ; ! 
+agromelioratsiya:agromelioratsiya N1 ; ! 
+agronom:agronom N1 ; ! 
+agronomik:agronomik A2 ; ! 
+agronomiya:agronomiya N1 ; ! 
+agronomlik:agronomli%{K%} N1 ;  ! 
+agrosanoat:agrosanoat N1 ; ! 
+agrotexnik:agrotexnik A2 ; ! 
+agrotexnik:agrotexnik N1 ; ! 
+agrouchastka:agrouchastka N1 ; ! 
+ada:ada N1 ; ! 
+adab:adab N1 ; ! 
+adabiyot:adabiyot N1 ; ! "literature" 
+adabiyotchi:adabiyotchi N1 ; ! 
+adabiyotchilik:adabiyotchili%{K%} N1 ;  ! 
+adabiyotshunos:adabiyotshunos N1 ; ! 
+adabiyotshunoslik:adabiyotshunosli%{K%} N1 ;  ! 
+adabiy:adabiy A2 ; ! "literary"
+adabiy%-publitsistik:adabiy%-publitsistik A2 ; ! 
+adabsiz:adabsiz A1 ; ! 
+adabsizlik:adabsizli%{K%} N1 ;  ! 
+adad:adad N1 ; ! 
+adadsiz:adadsiz A1 ; ! 
+adam:adam N1 ; ! 
+adaptatsiya:adaptatsiya N1 ; ! 
+adash:adash A2 ; ! 
+adash:adash V-IV ; ! 
+adash:adash N1 ; ! 
+adashtir:adashtir V-TV-CAUS ; ! 
+advokat:advokat N1 ; ! "lawyer" 
+advokatlik:advokatli%{K%} N1 ;  ! 
+advokatura:advokatura N1 ; ! 
+addoiy:addoiy A2 ; ! 
+adekvat:adekvat A2 ; ! 
+adekvatlik:adekvatli%{K%} N1 ;  ! 
+adyol:adyol N1 ; ! 
+adiba:adiba N1 ; ! 
+adi%-badi:adi%-badi N1→V ; ! 
+adi%-badi% ayt:adi%-badi% ayt V-IV ; ! 
+adi%-badi% aytish:adi%-badi% aytish V-IV-COOP ; ! 
+adib:adib N1 ; ! "writer"
+adiblik:adibli%{K%} N1 ;  ! 
+adibona:adibona ADV ; ! 
+adil:adil A1 ; ! 
+adillik:adilli%{K%} N1 ;  ! 
+adip:adip N1 ; ! 
+adipla:adipla V-TV ; ! 
+adir:adir N1 ; ! 
+adir%-gaza:adir%-gaza N1 ; ! 
+adirli:adirli A2 ; ! 
+adirlik:adirli%{K%} N1 ;  ! 
+adl:adl N1 ; ! 
+adl%-insof:adl%-insof N1 ; ! 
+adliya:adliya N1 ; ! 
+administrativ:administrativ A2 ; ! 
+administrator:administrator N1 ; ! 
+administratorlik:administratorli%{K%} N1 ;  ! 
+administratsiya:administratsiya N1 ; ! 
+admiral:admiral N1 ; ! 
+admirallik:admiralli%{K%} N1 ;  ! 
+adno:adno A1 ; ! 
+ado:ado N1 ; ! 
+ado% bul:ado% bul V-IV ; !
+ado% et:ado% et V-IV ; !
+ado% qil:ado% qil V-IV ; !
+adovat:adovat N1 ; ! 
+adovatli:adovatli A2 ; ! 
+adovatsiz:adovatsiz A1 ; ! 
+adovatsizlik:adovatsizli%{K%} N1 ;  ! 
+adovatchilik:adovatchili%{K%} N1 ;  ! 
+adoyi:adoyi N1 ; ! 
+adoyi% tamom% bul:adoyi% tamom% bul V-IV ; ! 
+adolat:adolat N1 ; ! 
+adolatli:adolatli A2 ; ! 
+adolatparvar:adolatparvar A2 ; ! 
+adolatparvar:adolatparvar N1 ; ! 
+adolatparvarlik:adolatparvarli%{K%} N1 ;  ! 
+adolatsiz:adolatsiz A1 ; ! 
+adolatsizlik:adolatsizli%{K%} N1 ;  ! 
+adras:adras N1 ; ! 
+adres:adres N1 ; ! 
+adresla:adresla V-TV ; ! 
+adyutant:adyutant N1 ; ! 
+aya:aya V-TV ; ! 
+ayo:ayo IJ ; ! 
+ayovli:ayovli A2 ; ! 
+ayovsiz:ayovsiz A1 ; ! 
+ayoz:ayoz N1 ; !
+ayoz:ayoz A2 ; ! 
+ayol:ayol N1 ; ! 
+ayol:ayol N1 ; ! "woman"
+ayolmand:ayolmand A2 ; ! 
+ayon:ayon A1 ; ! 
+ajab:ajab A1 ; ! 
+ajablan:ajablan V-IV ; ! 
+ajablanish:ajablanish N1-VN ; ! "surprise"
+ajablantir:ajablantir V-TV-CAUS ; ! 
+ajabo:ajabo ADV ; ! 
+ajabsin:ajabsin V-IV ; ! 
+ajabtovur:ajabtovur A1 ; ! 
+ajal:ajal N1 ; ! 
+ajam:ajam N1 ; ! 
+ajamiy:ajamiy A1 ; ! 
+ajdar:ajdar N1 ; ! 
+ajdargul:ajdargul N1 ; ! 
+ajdarnafs:ajdarnafs A2 ; ! 
+ajdarho:ajdarho N1 ; ! 
+ajdaho:ajdaho N1 ; ! 
+ajdahor:ajdahor N1 ; ! 
+ajdod:ajdod N1 ; ! "ancestor" 
+ajz:ajz N1 ; ! 
+aji%-aji:aji%-aji N1 ; ! 
+aji%-aji% qil:aji%-aji% qil V-IV ; ! 
+ajib:ajib A1 ; ! 
+aji%-buji:aji%-buji A1 ; ! 
+ajiva:ajiva A2 ; ! 
+ajiva%-jinni:ajiva%-jinni A2 ; ! 
+ajin:ajin N1 ; ! 
+ajina:ajina N1 ; ! 
+ajnabiy:ajnabiy A2 ; ! 
+ajnos:ajnos N1 ; ! 
+ajoyib:ajoyib A1 ; ! "suprising"
+ajoyiblik:ajoyibli%{K%} N1 ;  ! 
+ajoyibot:ajoyibot N1 ; ! 
+ajoyib%-gʻaroyib:ajoyib%-gʻaroyib A2 ; ! 
+ajoyibxona:ajoyibxona N1 ; ! 
+ajr:ajr N1 ; ! 
+ajra:ajra V-IV ; ! 
+ajral:ajral V-IV-REFL ; ! 
+ajralish:ajralish N1-VN ; ! 
+ajralmas:ajralmas A2 ; ! 
+ajrat:ajrat V-TV-CAUS ; ! 
+ajratish:ajratish N1-VN ; ! 
+ajratqich:ajratqich N1 ; ! 
+ajratma:ajratma N1 ; ! "allotment"
+ajrash:ajrash V-IV-COOP ; ! 
+ajriq:ajriq N1 ; ! 
+ajriqzor:ajriqzor N1 ; ! 
+ajrim:ajrim N1 ; ! 
+ajrimlik:ajrimli%{K%} N1 ;  ! 
+ajuz%-majuz:ajuz%-majuz N1-PL ; ! 
+az:az PREP ; ! 
+az% roʻzi% azal:az% roʻzi% azal ADV ; !
+aza:aza N1 ; ! 
+azab:azab N1 ; !
+azavozlik:azavozli%{K%} N1 ;  ! 
+azador:azador N1 ; ! 
+azadorlik:azadorli%{K%} N1 ;  ! 
+azayimxon:azayimxon N1 ; ! 
+azayimxonlik:azayimxonli%{K%} N1 ;  !
+azal:azal N1 ; ! "distant past"
+azalgi:azalgi A2 ; ! 
+azaliy:azaliy A2 ; ! 
+azamat:azamat A2 ; ! 
+azamat:azamat N1 ; ! 
+azamatlik:azamatli%{K%} N1 ;  ! 
+azbaroyi:azbaroyi PREP ; ! 
+azza%-bazza:azza%-bazza ADV ; ! 
+aziz:aziz A2 ; ! 
+azim:azim A2 ; ! 
+azimut:azimut N1 ; ! 
+aziyat:aziyat N1 ; ! 
+azl:azl A1→V ; ! 
+azl% qil:azl% qil V-TV ; ! 
+azl% et:azl% et V-TV ; ! 
+azm:azm N1 ; ! 
+azmqor:azmqor A1 ; ! 
+azmoyish:azmoyish N1 ; ! 
+azmoyish% ol:azmoyish% ol V-TV ; ! 
+azob:azob N1 ; ! "torture"
+azobla:azobla V-TV ; ! 
+azoblan:azoblan V-IV-REFL ; ! 
+azozil:azozil N1 ; ! 
+azon:azon N1 ; ! 
+azonlab:azonlab ADV ; ! 
+azonchi:azonchi N1 ; ! 
+azop:azop N1 ; !
+azot:azot N1→V ; ! 
+azot% ko%’tar:azot% ko%’tar V-TV ; ! 
+azot:azot N1 ; ! "nitrogen"
+azotla:azotla V-TV ; ! 
+azotli:azotli A2 ; ! 
+azroil:azroil N1 ; ! 
+aztahidil:aztahidil ADV ; ! 
+ay:ay IJ ; ! 
+ayb:ayb N1 ; ! 
+aybaki:aybaki N1 ; ! 
+aybdor:aybdor A1 ; ! 
+aybdor:aybdor N1 ; ! 
+aybdorlik:aybdorli%{K%} N1 ;  ! 
+aybla:aybla V-TV ; ! "to blame"
+ayblanuvchhi:ayblanuvchhi N1 ; ! 
+ayblash:ayblash V-TV-COOP ; ! 
+ayblash:ayblash N1-VN ; ! 
+aybli:aybli A2 ; ! 
+ayblov:ayblov N1-VN ; ! 
+ayblovchhi:ayblovchhi N1 ; ! 
+aybnoma:aybnoma N1 ; ! 
+aybona:aybona N1 ; ! 
+aybsiz:aybsiz A1 ; ! 
+aybsizlik:aybsizli%{K%} N1 ;  ! 
+aybsit:aybsit V-TV ; ! 
+ayvon:ayvon N1 ; ! "veranda"
+ayyom:ayyom N1 ; ! 
+ayyor:ayyor N1 ; ! 
+ayyorgarlik:ayyorgarli%{K%} N1 ;  ! 
+ayyorgarchilik:ayyorgarchili%{K%} N1 ;  ! 
+ayyorlarcha:ayyorlarcha ADV ; ! 
+ayyorlik:ayyorli%{K%} N1 ;  ! 
+ayyorona:ayyorona A1 ; ! 
+ayyorcha:ayyorcha ADV ; ! 
+ayil:ayil N1 ; ! 
+ayir:ayir V-TV ; ! 
+ayirbosh:ayirbosh N1 ; ! 
+ayirboshla:ayirboshla V-TV ; ! 
+ayirgich:ayirgich N1 ; ! 
+ayirish:ayirish N1-VN ; ! 
+ayirma:ayirma N1 ; ! 
+ayirmali:ayirmali A1 ; ! 
+ayirmachilik:ayirmachili%{K%} N1 ;  ! 
+ayirtir:ayirtir V-TV-CAUS ; ! 
+ayiq:ayiq N1 ; ! "bear"
+ayiqmurut:ayiqmurut N1 ; ! 
+ayiqtovon:ayiqtovon N1 ; ! 
+ayla:ayla V-TV ; ! 
+aylan:aylan V-IV ; ! "to spin"
+aylana:aylana N1 ; !
+aylanay:aylanay N1-ADR ; ! 
+aylanish:aylanish V-IV-COOP ; ! 
+aylanish:aylanish N1-VN ; ! 
+aylanma:aylanma A2 ; ! 
+aylanma:aylanma N1 ; ! " Whirl"
+aylanmachoq:aylanmachoq A2 ; ! 
+aylantir:aylantir V-TV-CAUS ; ! 
+aylash:aylash V-TV-COOP ; !
+aymash:aymash V-IV ; ! 
+aymoq:aymoq N1 ; ! 
+aymoqi:aymoqi A2 ; ! 
+aymoqi:aymoqi N1 ; ! 
+ayn:ayn A2 ; ! 
+ayna:ayna V-IV ; ! 
+aynama:aynama A2 ; ! 
+aynamachi:aynamachi A2 ; ! 
+aynamachilik:aynamachili%{K%} N1 ;  ! 
+aynan:aynan A1 ; ! 
+aynan:aynan ADV1 ; !
+aynat:aynat V-TV-CAUS ; ! 
+ayni:ayni A1 ; ! "exact"
+ayni:ayni V-IV ; ! 
+aynit:aynit V-TV-CAUS ; ! 
+ayniyat:ayniyat N1 ; ! 
+ayniqsa:ayniqsa ADV ; ! "specially"
+ayra:ayra N1→V ; ! 
+ayra% buyril:ayra% buyril V-IV ; ! 
+ayra% sol:ayra% sol V-IV ; !
+ayra% tush:ayra% tush V-IV ; ! 
+ayri:ayri N1 ; !
+ayri:ayri A2 ; !
+ayril%-ayril:ayril%-ayril N1→V ; ! 
+ayril%-ayril% boʻlib% ket:ayril%-ayril% boʻlib% ket V-IV ; !
+ayril:ayril V-IV-REFL ; ! 
+ayrilish:ayrilish V-IV-COOP ; ! 
+ayrilish:ayrilish N1-VN ; ! 
+ayrilik:ayrili%{K%} N1 ;  ! 
+ayrilmas:ayrilmas A2 ; ! 
+ayrim:ayrim A1 ; ! "separate"
+ayrim%-ayrim ADV ; !
+ayrimlik:ayrimli%{K%} N1 ;  ! 
+ayricha:ayricha A1 ; ! 
+ayron:ayron N1 ; ! 
+ayronchi:ayronchi N1 ; ! 
+ayt:ayt V-TV ; ! "to say"
+aytganday:aytganday IJ ; ! ""
+aytganday:aytganday PAREN ; ! 
+aytgandek:aytgandek PAREN ; ! 
+aytgancha:aytgancha PAREN ; ! 
+aytilish:aytilish N1-VN ; ! 
+aytish:aytish V-IV-COOP ; ! 
+aytish:aytish N1-VN ; ! 
+aytishi% boʻyicha:aytishi% boʻyicha CA ; ! ""
+aytishicha:aytishicha CA ; ! "according to"
+aytishqoq:aytishqoq N1 ; ! 
+aytkan% bilan:aytkan% bilan CA ; ! ""
+aytmoqchi:aytmoqchi PAREN ; ! 
+ayttir:ayttir V-TV-CAUS ; ! 
+aytuvli:aytuvli A2 ; ! 
+aytqich:aytqich A2 ; ! 
+aysh:aysh N1 ; ! 
+ayshgoh:ayshgoh N1 ; ! 
+aysh%-ishrat:aysh%-ishrat N1 ; ! 
+ayyuhannos:ayyuhannos INTERJ ; ! 
+ayyuhannos:ayyuhannos N1 ; ! 
+ayyuhannos% sol:ayyuhannos% sol V-IV ; !
+ayyuhannos% tort:ayyuhannos% tort V-IV ; ! 
+ayqash:ayqash V-IV ; ! 
+ayqash% uyqash:ayqash% uyqash A2 ; ! 
+ayqir:ayqir V-IV ; ! 
+ayqish%-tuyqush:ayqish%-tuyqush A2 ; !
+aygʻiz:aygʻiz N1 ; ! 
+aygʻir:aygʻir N1 ; ! 
+aygʻirqiyoq:aygʻirqiyoq N1 ; ! 
+aygʻoq:aygʻoq N1 ; !
+aygʻoq:aygʻoq A2 ; ! 
+aygʻoqla:aygʻoqla V-TV ; ! 
+aygʻoqchi:aygʻoqchi N1 ; ! 
+aygʻoqchilik:aygʻoqchili%{K%} N1 ;  ! 
+ayhay:ayhay IJ ; ! 
+ayhayla:ayhayla V-IV ; ! 
+aka:aka N1 ; ! 
+akavachcha:akavachcha N1 ; ! 
+akademizm:akademizm N1 ; ! 
+akademik:akademik A2 ; ! 
+akademik:akademik N1 ; ! 
+akademiya:akademiya N1 ; ! "academy"
+akala:akala V-IV ; ! 
+akas:akas N1 ; !
+akasi:akasi N1-ADR ; ! 
+aka%-singil:aka%-singil N1 ; ! 
+aka%-uka:aka%-uka N1 ; ! 
+akatsiya:akatsiya N1 ; ! 
+akashak:akashak A2 ; ! 
+akbar:akbar A2 ; ! 
+akvarel:akvarel N1 ; ! 
+akvarium:akvarium N1 ; ! 
+akildoq:akildoq N1 ; ! 
+akilla:akilla V-IV ; ! 
+akillat:akillat V-TV-CAUS ; ! 
+akkordeon:akkordeon N1 ; ! 
+akkordeonchi:akkordeonchi N1 ; ! 
+akkreditiv:akkreditiv N1 ; ! 
+akkumulyator:akkumulyator N1 ; ! 
+akobir:akobir N1 ; ! 
+akrobat:akrobat N1 ; ! 
+akrobatika:akrobatika N1 ; ! 
+akrobatik:akrobatik A2 ; ! 
+akrobatchi:akrobatchi N1 ; ! 
+aks:aks A1 ; ! 
+aks:aks N1 ; ! "reflection"
+aksa:aksa N1→V ; ! 
+aksa% ur:aksa% ur V-IV ; ! 
+aksar:aksar N1 ; ! "majority"
+aksariyat:aksariyat A1 ; ! "majority"
+aksariyati:aksariyati A1 ; ! "majority"
+aksiga:aksiga ADV ; ! 
+aksilinqilob:aksilinqilob N1 ; ! 
+aksilinqilobiy:aksilinqilobiy A2 ; ! 
+aksilinqilobchi:aksilinqilobchi N1 ; ! 
+aksilharakat:aksilharakat N1 ; ! 
+aksilharakatchi:aksilharakatchi N1 ; ! 
+aksincha:aksincha ADV ; !
+aksincha:aksincha PAREN ; ! 
+aksioma:aksioma N1 ; ! 
+aksir:aksir V-IV ; ! 
+aksirik:aksirik N1 ; ! 
+aksirish:aksirish N1-VN ; ! 
+aksirtir:aksirtir V-TV-CAUS ; ! 
+aksicha:aksicha ADV ; !
+aksicha:aksicha PAREN ; ! 
+akslan:akslan V-IV ; ! 
+akslik:aksli%{K%} N1 ;  ! 
+aks% xolda:aks% xolda CA ; ! "otherwise"
+aks% et:aks% et V-IV ; !
+aks% ettir:aks% ettir V-TV-CAUS ; ! 
+akt:akt N1 ; ! 
+aktyor:aktyor N1 ; ! "actor"
+aktyorlik:aktyorli%{K%} N1 ;  ! 
+aktiv:aktiv A1 ; ! 
+aktiv:aktiv N1 ; ! 
+aktivlash:aktivlash V-IV ; ! 
+aktivlashtir:aktivlashtir V-TV-CAUS ; ! 
+aktivlik:aktivli%{K%} N1 ;  ! 
+aktiniy:aktiniy N1 ; ! 
+aktrisa:aktrisa N1 ; ! 
+aktual:aktual A2 ; ! 
+aktuallik:aktualli%{K%} N1 ;  ! 
+akustika:akustika N1 ; ! 
+akustik:akustik A2 ; ! 
+akusher:akusher N1 ; ! 
+akusherka:akusherka N1 ; ! 
+akusherlik:akusherli%{K%} N1 ;  ! 
+aksent:aksent N1 ; ! 
+aksept:aksept N1 ; ! 
+aktseptlash:aktseptlash N1-VN ; ! 
+aksiz:aksiz N1 ; ! 
+aksioner:aksioner N1 ; ! 
+aksiya:aksiya N1 ; ! 
+aksiyador:aksiyador N1 ; ! 
+aksiyadorlik:aksiyadorli%{K%} N1 ;  ! "joint-stock"
+aksiyali:aksiyali A2 ; ! 
+aksiyachi:aksiyachi N1 ; ! 
+alay%-balay:alay%-balay ADV ; ! 
+alay%-bulay:alay%-bulay ADV ; ! 
+alaykum:alaykum IJ←IJ ; !
+alayno:alayno A1→A1 ; ! 
+alayno%-oshkor:alayno%-oshkor A1 ; ! 
+alayhillaʼna:alayhillaʼna A2 ; ! 
+alayxissalom:alayxissalom A2 ; ! 
+alak:alak N1 ; ! 
+alaka%-shalaka:alaka%-shalaka A1 ; ! 
+al:al MIST ; ! ""
+alalxusus:alalxusus ADV ; ! 
+alam:alam N1 ; ! 
+alamangiz:alamangiz A2 ; ! 
+alam%-achchiq:alam%-achchiq N1 ; ! 
+alamdiyda:alamdiyda A2 ; ! 
+alamdor:alamdor N1 ; ! 
+alamzada:alamzada A2 ; ! 
+alamzadalik:alamzadali%{K%} N1 ;  ! 
