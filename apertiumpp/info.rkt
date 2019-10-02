9c4aadb6996705c77a3ca00491e01973033cf028
Ilnar Salimzianov <ilnar@selimcan.org>
2019-10-02 05:03:33 +0300

    add dependencies

--- apertiumpp/info.rkt
+++ apertiumpp/info.rkt
@@ -1,6 +1,6 @@
 #lang info
 (define collection "apertiumpp")
-(define deps '("base" "rash"))
+(define deps '("base" "rash" "html-parsing" "brag" "sxml"))
 (define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
 (define scribblings '(("scribblings/apertiumpp.scrbl" ())))
 (define pkg-desc "Description Here")
